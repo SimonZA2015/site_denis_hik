a4aad7e4d460d6df86de77584405c38d0a3c0f80
SimonZA2015 <dtyushev2015@gmail.com>
2021-03-30 09:36:07 +0300

    -MenuPopup подправил -NavBar иконки перенес в проект сайта

--- src/data/config/text.js
+++ src/data/config/text.js
@@ -8,7 +8,7 @@ let listSpanProgrammingSlider = ['Писал на языках: Java(Androids ap
 
 let listTitleProjectSlider = ['Horse Book', '', ''];
 let listSpanProjectSlider = ['Нацеленно на обьеденение услуг для лошадей в одном приложении',];
-let listImageProjectSlider = ['https://lh3.googleusercontent.com/0x-6A_axaU8qYzaAlx-fIn2CmLLAEyCRhcYv2P-fF4vRLuvxQNnTs_WhlsU4U-i8NNCQ7O8kFIJ6tELkhoeP=w1920-h1080',]
+let listImageProjectSlider = ['https://lh3.googleusercontent.com/wd3KKmNOcKlOd4YZYEk1n7e4AEwkn0-HV6Ad37QuYDgk9w0ukmF7s1oGT7xFC_rbmLA_5We6a2qA5AAmoZOp7Z7gx9lFtyBG01GnfEzZJyHGbpXcoBZRt27Y2TbzDSxUDvWCtiFLn6Ypus_CHZsleZxc-q8BvzhqWxf4hH-ZEcd_k7lBB3x8L9kMyc2BKYVT7NepG4iZd0IbMdQVER5675UZ2HP3g7993u3l81aJN_WsaYIK2FA8p9soNQX7_aGW5CZtzAaXygbUoP0Ozm9BrLfFbkX8PcxiWcxF7q63cAuvrXn8PqV3PRqYZ8CbezX1bI2BCdJ_HVIx9ACqoJQ9tqH5RyPH96TiTWfDeg8jepOi_5uvBaJmsnVPZ1pqZvnxd6MyMuiOZ-F51T43LXrd20jrAVm-rx5c6AH-Grub6y4RckAPcO4UnviGUD5NUuCd7qOVDVMmEcKTGrVs-YKn9Rdfwy8hFNhmtW_lI3YG3W8OaHbW7cPO_8Jq8mCxuCQDHqU0m2AG8NwuTET88iP4TgLEeV2jZKw--GjpB_0UdWD2SmnDxiPj4EWN4TB3IwfnIoNj6IZr12swrIKBcPZx1zTAgSzH_d6M9NAmabZaQHDJjoLlr5YwTD-EyboWS1zn8lH9BY4xEoHwRgU22MtBCVJ12ALTKYaQTZaxWq4HIxni6glHAysOLWuNIf8fa2uqwuqjG2opnp_cRECiAtwvuwq5=w588-h315-no?authuser=0',]
 
 let listButtonsProjectSlider = {
     text: ['GitHub', ],
--- src/data/config/urls.js
+++ src/data/config/urls.js
@@ -1,5 +1,4 @@
 let vkUrl = 'http://vk.com/denis_hik';
 let instaUrl = 'http://instagram.com/denis_hik';
-let gitUrl = 'https://github.com/SimonZA2015?tab=repositories'
-
+let gitUrl = 'https://github.com/SimonZA2015?tab=repositories';
 export {instaUrl, vkUrl, gitUrl};
