06004b0642ae5f1e3e7d34d98cd9867fa2f3472b
Denis Hik <41349289+SimonZA2015@users.noreply.github.com>
2021-10-25 05:47:05 +0300

    -писпарвил ссылку на гит

--- src/data/config/urls.js
+++ src/data/config/urls.js
@@ -1,5 +1,5 @@
 let vkUrl = 'http://vk.com/denis_hik';
 let instaUrl = 'http://instagram.com/denis_hik';
-let gitUrl = 'https://github.com/SimonZA2015?tab=repositories';
+let gitUrl = 'https://github.com/Denis-hik?tab=repositories';
 let donatUrl = 'https://donat.denishik.ru'
 export {instaUrl, vkUrl, gitUrl, donatUrl};
