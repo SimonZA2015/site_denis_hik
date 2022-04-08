b32554fc620bb3d84d31c6c0836c623fee77ddb1
Denis Hik <41349289+SimonZA2015@users.noreply.github.com>
2022-04-08 14:24:41 +0300

    -Сделал VrChat блок

--- src/data/configs/text.js
+++ src/data/configs/text.js
@@ -12,12 +12,12 @@ export let headerHomeText = {
     span0: "Разрабочик разных проектов,",
     span1: "Тренер-наездник лошадей",
 }
-let listTitleProjectSlider = ['Horse Book', 'Узнать больше'];
-let listSpanProjectSlider = ['Нацеленно на обьеденение услуг для лошадей в одном приложении', 'Список проектов'];
+let listTitleProjectSlider = ['Horse Book', 'Проекты', 'VRChat'];
+let listSpanProjectSlider = ['Нацеленно на обьеденение услуг для лошадей в одном приложении', 'Список проектов', 'Список проектов для VR Chat'];
 let listImageProjectSlider = [
     hbImage,
     "https://lh3.googleusercontent.com/fife/AAWUweUw30hQsg5BPKoXL7EdEOffYln67kbzOptgCM73w_FJ1avy4-L6OdhZfVzoOsrXj-H5f5AGJ_HpS91EXuOnj5miu1e5yg4_k3tC7Ap4uUfegI8_fEJINpIgvIkP-mD_vcSa-0v-LewNB8zqWe2tJ6s1WXCrbCsZmiyy9IqQEj5mW5ZRd8UEBuB_4_qqUtVj4WAu15TK_ojv8vpUivXX2dDFt2iSSSCJNE4x_X-_EvSq1cOXyQkzs5JuJJRUkqWezQdvyGVD444NolX2LyoUuE0W4sfQdDYxDwg83QrXOOKGl7J8N_LrquXThEpRmF5qnELJENtwU7djKQIfoVLoig0al6a_ix_7u273T5csghV_vvtkPv4VBfuV1y6N5zoGp7lcfnhYLqUfxvdoQ3GwxqsLRbwMlgGFS8AsMqYjQkjF2lxkjjhG4H3ik37BZcjlsPhv_q1Q_55sTx5oI_BT-TjIySj-KCht1MrNcyRNp1rJEkl5CrhFfNLeAGrOvvYOmjM5c0jPnF93EzuyTI5EdB9vGWgboPX3b9gIKiXcEhisUdM2Ak45WHc1zye9HorgUjVrAIOBhRFVHk2hTpvoOHBFln4i-1Y0cKZQsIRXlBd0inTYxjJs_eheb8fOpNRtLt5VsIiIkIz99gPl0t_PlDAUYs3mDGLH7oxNOr3gXNGxrAvKq19ST3MBhAM1h8J4gkCG0Zllx1tTp2R-oGtQQO-gSbniZTPqin0=w1920-h947-ft",
-    false
+    "https://vrchat.denishik.ru/static/media/worldsBG.d75753825047bca8a2e0.png"
 ]
 
 let listButtonsProjectSlider = {
@@ -28,7 +28,8 @@ let listButtonsProjectSlider = {
     ],
     url: [
         'https://vk.com/app7683801',
-        'https://projects.denishik.ru'
+        'https://projects.denishik.ru',
+        'https://vrchat.denishik.ru',
     ]
 }
 
